9e0b2ac6a634334bf0df966372c17da18d212f8c
bjimenez52 <bjimenez52@students.mchenry.edu>
2025-01-23 14:54:40 -0800

    Add files via upload

---
+++ Python/Data_Types Changed.py
@@ -0,0 +1,21 @@
+age=30
+name = "Ruby"
+
+print("My age is", age)
+print("The name is", name)
+
+
+x = 15
+print("literal:", x)
+
+y = 0
+print("integer:", y)
+
+z = 13.5
+print("float:", z)
+
+name = "Ruby"
+print("string:", name)
+
+is_active = False
+print("Boolean:", is_active)
