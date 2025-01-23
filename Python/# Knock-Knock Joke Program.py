a96c0f7d015561b0eb1d38224cfaef1fbbe5d4b9
bjimenez52 <bjimenez52@students.mchenry.edu>
2025-01-23 14:04:05 -0800

    Add files via upload

---
+++ Python/# Knock-Knock Joke Program.py
@@ -0,0 +1,12 @@
+# Knock-Knock Joke Program
+print("Knock, Knock.")
+print("Who's there?")
+print("Cat.")
+print("Cat who?")
+print("Cat-astrophe! \nI just knocked over a glass of water!")
+
+print()
+# As one line of code using escape characters
+print("Knock, Knock.\nWho's there?\nCat.\nCat who?\nCat-astrophe! I just knocked over a glass of water!")
+
+print("Cats", "don't", "have", "a", "sweet", "tooth", sep="-" )
