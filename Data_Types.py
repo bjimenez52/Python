42ca289eba37948944836b496c375a1d8c76f2f3
bjimenez52 <bjimenez52@students.mchenry.edu>
2025-01-23 14:46:47 -0800

    Add files via upload

---
+++ Data_Types.py
@@ -0,0 +1,21 @@
+age=25
+name = "John"
+
+print("My age is",age)
+print("The name is",name)
+
+
+x = 10
+print("literal:",x)
+
+y = -20
+print("integer:",y)
+
+z = 15.5
+print("float:",z)
+
+name = "John"
+print("string:",name)
+
+is_active = True
+print("Boolean:",is_active)
