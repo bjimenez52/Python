3fcbea1172cf87d80a3d576596ccbe3e90f4e2a7
bjimenez52 <bjimenez52@students.mchenry.edu>
2025-02-20 20:01:37 -0600

    Add files via upload

---
+++ Python/madlib.py
@@ -0,0 +1,38 @@
+'''
+Homework madlib
+Name: Bianca Jimenez
+github link: https://github.com/bjimenez52/Python/tree/main/Python
+'''
+def custom_song(pronoun1,pronoun2,noun1,adjective1,noun2,adjective2,noun3,verb):
+    print(f"{pronoun1} are {pronoun2} {noun1} {pronoun2} only {noun1}")
+    print(f"{pronoun1} make me {adjective1} when {noun2} are {adjective2}")
+    print(f"You'll never know {noun3}, how much I love {pronoun1}")
+    print(f"Please don't take {pronoun2} {noun1} away")
+    print()
+    print(f"The other night {noun3}, as I lay sleeping")
+    print(f"I dreamed I held {pronoun1} in {pronoun2} arms")
+    print(f"When I awoke {noun3}, I was mistaken")
+    print(f"And I hung {pronoun2} head and {verb}")
+    print()
+    print(f"{pronoun1} are {pronoun2} {noun1}, {pronoun2} only {noun1}")
+    print(f"{pronoun1} make me {adjective1} when {noun2} are {adjective2}")
+    print(f"You'll never know {noun3}, how much I love {pronoun1}")
+    print(f"Please don't take {pronoun2} {noun1} away")
+    print()
+    print(f"I'll always love {pronoun1} and make {pronoun1} {adjective1}")
+    print(f"If {pronoun1} will only say the same")
+    print(f"But if {pronoun1} leave me to love another")
+    print(f"You'll regret it all some day")
+    print()
+    print(f"{pronoun1} are {pronoun2} {noun1}, {pronoun2} only {noun1}")
+    print(f"{pronoun1} make me {adjective1} when {noun2} are {adjective2}")
+
+pronoun1 = input("Enter a pronoun: ")
+pronoun2 = input("Enter another pronoun: ")
+noun1 = input("Enter a noun: ")
+adjective1 = input("Enter an adjective: ")
+noun2 = input("Enter another another noun: ")
+adjective2 = input("Enter another adjective: ")
+noun3 = input("Enter another noun: ")
+verb = input("Enter a verb: ")
+custom_song(pronoun1,pronoun2,noun1,adjective1,noun2,adjective2,noun3,verb)
